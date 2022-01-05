2fe6d35c921910018d94f3b14f97ce3d82664ad2
lrakai <lmrakai@gmail.com>
2022-01-04 21:44:32 -0700

    Make sample more copy-pasteable

--- src/scratch.py
+++ src/scratch.py
@@ -76,11 +76,17 @@ def handler(event, context):
 
     return output == 'True'
 
+def create_new_session(event):
+    return boto3.Session(
+        aws_access_key_id=event['credentials']['credential_id'],
+        aws_secret_access_key=event['credentials']['credential_key'],
+      	region_name=event['region_id']
+    )
 
 def timed_handler():
     start = time.time()
 
-    result = handler(None, None)
+    handler({}, None)
 
     end = time.time()
     print(end - start)
