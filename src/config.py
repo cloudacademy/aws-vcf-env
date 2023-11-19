f01bb43eabc34b0d909cf51e8258ff112f698f84
lrakai <lmrakai@gmail.com>
2023-11-18 23:08:52 -0700

    remove separate credential handler

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 import os
 
 CONFIG = {
-    'client_id'     : os.environ.get('AWS_CLIENT_ID'),
-    'client_secret' : os.environ.get('AWS_CLIENT_SECRET'),
-    'region_name'   : os.environ.get('AWS_REGION', 'us-west-2')
+    'credentials': {
+        'credential_id': os.environ.get('AWS_CLIENT_ID'),
+        'credential_key': os.environ.get('AWS_CLIENT_SECRET'),
+    },
+    'region_id': os.environ.get('AWS_REGION', 'us-west-2'),
 }
